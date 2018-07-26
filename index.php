19b406c13b09d13c738088514d457bf42eb44bd7
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:28:11 +0200

    variable

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo7</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $name ='cyril';
+    $lastname ='jardin';
+    $age ='18';
+    echo "Bonjour $name $lastname ,tu as $age ans";
+     ?>
+   </p>
+  </body>
+</html>
